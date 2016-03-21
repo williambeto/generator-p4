6601d5ad9cd355185241563368304de1141c2054
Plano4 Web <Plano4 Web>
2016-03-21 16:37:29 -0300

    (publicação inicial)

---
+++ README.md
@@ -0,0 +1,61 @@
+# Foundation 6 + Sass com Libsass + Grunt
+
+Este é o scaffold do Zurb Foundation 6 usado na Plano4.com.br
+
+### Nota:
+
+** Este é o scaffold do Foundation 6 usado na Plano4.com.br baseado no projeto de JULIAN ĆWIRKO [Foundation 5 and Yeoman generator-zf5](http://julian.io/foundation-5-and-yeoman/) 
+
+## Yo P4!
+* Zurb Foundation 6
+* Motion UI Sass Library
+* Sass com Libsass
+* Autoprefixer
+* Compass Functions com 'compass-mixins' (https://github.com/Igosuki/compass-mixins)
+* BrowserSync Live Reloading
+* Font Awesome
+* Automação de tarefas com Grunt
+
+## Instalação rápida
+
+#### Instalação
+
+Instale o Yeoman:
+
+```
+npm install -g yo
+```
+
+Instale o generator-p4 [npm](https://www.npmjs.com/package/generator-p4):
+
+```
+npm install -g generator-p4
+```
+
+#### Criando o Projeto
+
+Navegue até a pasta do projeto e inicialize o generator-p4:
+
+```
+yo p4
+```
+
+#### Iniciando o Projeto
+
+Navegue até a pasta do projeto e digite `grunt` para iniciar:
+
+```
+grunt
+```
+
+Para exportar os arquivos para produção:
+
+```
+grunt publish
+```
+
+A versão finalizada será exportada para a pasta `dist`
+
+## Licença
+
+MIT
---
+++ app/index.js
@@ -0,0 +1,45 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var mkdirp = require('mkdirp');
+
+var P4Generator = yeoman.generators.Base.extend({
+    init: function () {
+        this.pkg = require('../package.json');
+        this.config.save();
+
+        this.on('end', function () {
+            //console.log('ACABOU!!');
+            //this.installDependencies();
+        });
+    },
+    
+    appFiles: function () {
+        mkdirp('dist');
+        mkdirp('app');
+        this.template('bower.json', 'bower.json');
+        this.template('package.json', 'package.json');
+        this.template('Gruntfile.js', 'Gruntfile.js');
+        this.copy('.jshintrc', '.jshintrc');
+        this.copy('.bowerrc', '.bowerrc');
+        this.copy('gitignore', '.gitignore');
+        this.copy('README.md', 'README.md');
+        this.template('index.html', 'app/index.html');
+        mkdirp('app/fonts');
+        mkdirp('app/images');
+        mkdirp('app/js');
+        mkdirp('app/css');
+        mkdirp('app/scss');
+        this.copy('scss/app.scss', 'app/scss/app.scss');
+        this.copy('scss/_settings.scss', 'app/scss/_settings.scss');
+        this.template('scss/_appstyles.scss', 'app/scss/_appstyles.scss');
+        this.copy('js/app.js', 'app/js/app.js');
+    },
+    
+    install: function () {
+        this.installDependencies();
+    }
+});
+
+module.exports = P4Generator;
